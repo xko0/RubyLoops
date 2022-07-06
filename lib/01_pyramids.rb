1259306e5d608c76f42fa452b7c9e7390fe62bd4
xko <germain.rgb@hotmail.fr>
2022-07-06 16:43:22 +0200

    wtf pyramid

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -1,20 +1,25 @@
-def pyramid
+def wtf_pyramid
     puts "Hi welcome to my pyramide ! How many floors do you want ?"
     print "->"
     floors = gets.chomp.to_i
     until floors <= 25 and floors > 0
-    puts "Make it less than 26 please"
-    print "->"
-    floors = gets.chomp.to_i
-    return floors
+        puts "Make it less than 26 please"
+        print "->"
+        floors = gets.chomp.to_i
+        return floors
     end
     print "here it comes !"
     for n in 0..floors
-    (floors-n).times {print " "}
-	n.times {print "#"}
-    (n-1).times {print "#"}
-	puts
+        (floors-n).times {print " "}
+	    n.times {print "#"}
+        (n-1).times {print "#"}
+        puts
+    end
+    for n in 1..floors
+        n.times {print " "}
+        (floors-n).times {print "#"}
+        (floors-n-1).times {print "#"}
+        puts
     end
 end
-
-pyramid
+wtf_pyramid
