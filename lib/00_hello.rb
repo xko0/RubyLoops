e530974425af4dfb7eca982bcff238e6ac58b450
xko <germain.rgb@hotmail.fr>
2022-07-06 15:21:32 +0200

    say hello first name

--- lib/00_hello.rb
+++ lib/00_hello.rb
@@ -1,5 +1,17 @@
-def say_hello
-    puts "Hello world !"
+def ask_first_name
+    puts "Hi, what's your first name ?"
+    print "->"
+    first_name = gets.chomp
+    return first_name
 end
 
-say_hello
+def say_hello(first_name)
+    puts "Hello, #{first_name}!"
+end
+  
+def perform
+    first_name = ask_first_name
+    say_hello(first_name)
+end
+  
+perform
